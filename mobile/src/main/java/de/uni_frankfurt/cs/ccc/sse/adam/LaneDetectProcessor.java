dfc028cebe59a87dd65615b634f2974e6983c3c3
Paul Idstein <paul@idstein.info>
2016-03-03 09:18:00 +0100

    Refactor by Clean Code principles

--- mobile/src/main/java/de/uni_frankfurt/cs/ccc/sse/adam/LaneDetectProcessor.java
+++ mobile/src/main/java/de/uni_frankfurt/cs/ccc/sse/adam/LaneDetectProcessor.java
@@ -5,8 +5,8 @@ import org.opencv.core.Point;
 import org.opencv.core.Scalar;
 import org.opencv.imgproc.Imgproc;
 
-import java.util.LinkedList;
-import java.util.Queue;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.logging.Logger;
 
 import static java.lang.Math.PI;
@@ -15,14 +15,16 @@ public class LaneDetectProcessor implements MatrixProcessor {
 
     private static final Logger logger = Logger.getLogger(LaneDetectProcessor.class.getName());
 
-    private static final double rho = 1, theta = PI/180;
+    private static final double rho = 1, theta = PI / 180;
 
-    private static final double CANNY_MIN_THRESHOLD = 200,//edge detector mininum hysteresis threshold
-            CANNY_MAX_THRESHOLD = 550,//edge detector maximum hysteresis threshold
-            HOUGH_MIN_LINE_LENGTH = 25, //remove lines shorter than this threshold
-            HOUGH_MAX_LINE_GAP = 200; //join lines
+    private static final double CANNY_MIN_THRESHOLD = 1,//edge detector mininum hysteresis threshold
+            CANNY_MAX_THRESHOLD = 50,//edge detector maximum hysteresis threshold
+            HOUGH_MIN_LINE_LENGTH = 40, //remove lines shorter than this threshold
+            HOUGH_MAX_LINE_GAP = 300; //join lines
 
-    private static final int HOUGH_THRESHOLD = 25;     //line approval vote threshold
+    private static final int HOUGH_THRESHOLD = 40;     //line approval vote threshold
+    List<Line> lines = new ArrayList<>();
+    Mat lanes;
 
     @Override
     public Mat process(Mat input) {
@@ -33,11 +35,12 @@ public class LaneDetectProcessor implements MatrixProcessor {
         Imgproc.equalizeHist(grayscale, grayscale);
 
         Imgproc.bilateralFilter(grayscale, input, 5, 10, 10);
-        Imgproc.Canny(input, input, CANNY_MIN_THRESHOLD, CANNY_MAX_THRESHOLD, 3, false);
+        Imgproc.Canny(input, input, CANNY_MIN_THRESHOLD, CANNY_MAX_THRESHOLD);//, 3, false);
         Mat newLanes = new Mat();
         Imgproc.HoughLinesP(input, newLanes, rho, theta,
                 HOUGH_THRESHOLD, HOUGH_MIN_LINE_LENGTH, HOUGH_MAX_LINE_GAP);
         lines.clear();
+
         for (int i = 0; i < newLanes.rows(); i++) {
             //double[] vec = lanes.get(0, i);
             double[] vec = newLanes.get(i, 0);
@@ -46,31 +49,50 @@ public class LaneDetectProcessor implements MatrixProcessor {
                     x2 = vec[2],
                     y2 = vec[3];
 
-            int angle = (int)(Math.atan((y1-y2)/(x2-x1))*180/Math.PI);
-            if(Math.abs(angle) <20)
+            int angle = (int) (Math.atan((y1 - y2) / (x2 - x1)) * 180 / Math.PI);
+            if (Math.abs(angle) < 20 || Math.abs(angle) >= 85)
                 continue;
 
             Line a = new Line();
             a.start = new Point(x1, y1);
             a.end = new Point(x2, y2);
             lines.add(a);
+
         }
 
+        List<Point> vanishingPointCandidates = new ArrayList<>();
+        for (int i = 0; i < lines.size(); i++) {
+            for (int j = i + 1; j < lines.size(); j++) {
+                final Line a = lines.get(i), b = lines.get(j);
+                final Point intersection = computeIntersect(a.start.x, a.start.y, a.end.x, a.end.y,
+                        b.start.x, b.start.y, b.end.x, b.end.y);
+                if (intersection != null)
+                    vanishingPointCandidates.add(intersection);
+            }
+        }
         return input;
     }
-
-    class Line {
-        Point start, end;
-    }
-
-    Queue<Line> lines = new LinkedList<>();
-
-    Mat lanes;
 
     public void drawLanes(Mat input) {
         for (Line line : lines) {
             Imgproc.line(input, line.start, line.end, new Scalar(0, 128, 128), 3);
         }
     }
+
+    Point computeIntersect(final double x1, final double y1, final double x2, final double y2,
+                           final double x3, final double y3, final double x4, final double y4) {
+        double d = (y4 - y3) * (x2 - x1) - (x4 - x3) * (y2 - y1);
+        if (d == 0.0) { // Lines are parallel.
+            return null;
+        }
+
+        return new Point(((x1 * y2 - y1 * x2) * (x3 - x4) - (x1 - x2) * (x3 * y4 - y3 * x4)) / d,
+                ((x1 * y2 - y1 * x2) * (y3 - y4) - (y1 - y2) * (x3 * y4 - y3 * x4)) / d);
+    }
+
+    class Line {
+        Point start, end;
+    }
+
 
 }
--- mobile/src/main/java/de/uni_frankfurt/cs/ccc/sse/adam/StreetViewActivity.java
+++ mobile/src/main/java/de/uni_frankfurt/cs/ccc/sse/adam/StreetViewActivity.java
@@ -6,7 +6,6 @@ import android.util.Log;
 
 import org.opencv.android.BaseLoaderCallback;
 import org.opencv.android.CameraBridgeViewBase;
-import org.opencv.android.LoaderCallbackInterface;
 import org.opencv.core.Mat;
 import org.opencv.video.BackgroundSubtractor;
 import org.opencv.video.Video;
@@ -22,41 +21,26 @@ import static org.opencv.android.OpenCVLoader.initDebug;
  * An example full-screen activity that shows and hides the system UI (i.e.
  * status bar and navigation/system bar) with user interaction.
  */
-public class StreetViewActivity extends Activity implements CameraBridgeViewBase.CvCameraViewListener2 {
+public class StreetViewActivity extends Activity {
     private static final String TAG = "StreetViewActivity";
-    private CameraBridgeViewBase mDashcamView;
 
-    private Mat lanes;
     /**
      * Load OpenCV Manager
      */
-    private BaseLoaderCallback loaderCallback = new BaseLoaderCallback(this) {
-        @Override
-        public void onManagerConnected(int status) {
-            switch (status) {
-                case LoaderCallbackInterface.SUCCESS: {
-                    Log.i(TAG, "OpenCV library loaded successfully");
-                    mDashcamView.enableView();
-                    lanes = new Mat();
-                }
-                break;
-                default: {
-                    super.onManagerConnected(status);
-                }
-                break;
-            }
-        }
-    };
+    private BaseLoaderCallback loaderCallback = new OpenCVLoaderCallback(this);
+    private CameraHandler cameraHandler;
+    private CameraBridgeViewBase mDashcamView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.dashcam);
-
         mDashcamView = (CameraBridgeViewBase) findViewById(R.id.dashcam_java_surface_view);
-        mDashcamView.setCvCameraViewListener(this);
+        cameraHandler = new CameraHandler();
+        mDashcamView.setCvCameraViewListener(cameraHandler);
         mDashcamView.setMaxFrameSize(640,480);
+        if (BuildConfig.DEBUG)
+            mDashcamView.enableFpsMeter();
     }
 
     @Override
@@ -76,41 +60,7 @@ public class StreetViewActivity extends Activity implements CameraBridgeViewBase
         }
     }
 
-    @Override
-    public void onCameraViewStarted(int width, int height) {
-        if (BuildConfig.DEBUG)
-            mDashcamView.enableFpsMeter();
-        backgroundSubtractor = Video.createBackgroundSubtractorMOG2();
-    }
-
-    @Override
-    public void onCameraViewStopped() {
-
-    }
-
-    BackgroundSubtractor backgroundSubtractor;
-
-    LaneDetectProcessor laneDetector = new LaneDetectProcessor();
-    int count = 0;
-
-    @Override
-    public Mat onCameraFrame(CameraBridgeViewBase.CvCameraViewFrame inputFrame) {
-        Mat input = inputFrame.rgba();
-        Mat roi = input.rowRange((int) (input.rows()*0.48), input.rows());
-        Mat mask = new Mat();
-        backgroundSubtractor.apply(roi, mask);
-        Mat output = new Mat();
-        roi.copyTo(output, mask);
-        //if(fps % 25 == 0) {
-        //if(count%25==0) {
-            laneDetector.process(output);
-        //    count = 0;
-        //} else {
-        //    count ++;
-        //}
-        laneDetector.drawLanes(roi);
-        //}
-
-        return input;
+    void enableOpenCV() {
+        mDashcamView.enableView();
     }
 }
