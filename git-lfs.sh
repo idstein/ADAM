1a58454b7eca6a319e107aa7eacf71c4d05f9a8d
Paul Idstein <paul@idstein.info>
2016-03-04 13:01:56 +0100

    Fix Travis: Use git lfs as Travis image does not support it out of the box to download our big movies - Replace deprecated method init with install

--- git-lfs.sh
+++ git-lfs.sh
@@ -2,5 +2,5 @@
 #set -e
 wget https://github.com/github/git-lfs/releases/download/v1.1.2/git-lfs-linux-amd64-1.1.2.tar.gz
 mkdir git-lfs && tar vxzf git-lfs-linux-amd64-1.1.2.tar.gz -C git-lfs --strip=1
-git-lfs/git-lfs init
+git-lfs/git-lfs install
 git-lfs/git-lfs fetch
