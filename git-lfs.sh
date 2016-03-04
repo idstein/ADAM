4d814881c5bda7a6f0ba2590b15dc3d849a8aaa4
Paul Idstein <paul@idstein.info>
2016-03-04 12:38:06 +0100

    Fix Travis: Use git lfs as Travis image does not support it out of the box to download our big movies - Extraction requires folder to exist - Make it more verbose for build log

--- git-lfs.sh
+++ git-lfs.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #set -e
 wget https://github.com/github/git-lfs/releases/download/v1.1.2/git-lfs-linux-amd64-1.1.2.tar.gz
-tar -xzf git-lfs-linux-amd64-1.1.2.tar.gz -C git-lfs
+mkdir git-lfs && tar vxzf git-lfs-linux-amd64-1.1.2.tar.gz -C git-lfs --strip=1
 git-lfs/git-lfs init
 git-lfs/git-lfs fetch
